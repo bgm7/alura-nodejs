74bdf712fb3af57cfcaa88e98df4aba47e34ec4a
Brunno Mattos <brunno.mattos@yahoo.com.br>
2016-07-03 14:27:40 -0400

    configurando serviço para utilizar variavel de ambiente process.env.PORT ou a porta 3000 por padrão

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = require('./config/express')();
 var rotasProdutos = require('./app/routes/produtos')(app);
+var porta = process.env.PORT || 3000;
 
-app.listen(3000, function(){
-	console.log("rodando");
+
+app.listen(porta, function(){
+	console.log("rodando na porta: " + porta);
 });
