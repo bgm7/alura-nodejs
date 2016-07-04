c5c318b65ffcb0ede69f7851c74fc7d1c7b7e6ed
Brunno Mattos <brunno.mattos@yahoo.com.br>
2016-07-04 00:50:59 -0300

    Alteracao do html para construir tabela de produtos dinamicamente

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,8 @@
 
+var results = require('../../mock/produto/produtos.json');
+
 module.exports = function(app){
 	app.get('/produtos', function(req, res){
-		res.render("produtos/lista");
+		res.render("produtos/lista", {lista:results});
 	});
 }
